8496dd4c2fe9808c25adfc1c032a741fac3bb6f4
Gabriel Camacho <gabriel.camacho@smlbrasil.com.br>
2021-07-20 20:55:30 -0300

    Initial Project

---
+++ Zeev.AsposeBarcode/Zeev.AsposeBarcode.Library/ReadBarcode.cs
@@ -0,0 +1,48 @@
+﻿using Aspose.BarCode.BarCodeRecognition;
+using System;
+using System.Drawing;
+using System.IO;
+
+namespace Zeev.AsposeBarcode.Library
+{
+    public class ReadBarcode
+    {
+        public static void Read()
+        {
+            Aspose.BarCode.License license = new Aspose.BarCode.License();
+            license.SetLicense(@"D:\GIT-Estudo\Zeev.AsposeBarcode\Zeev.AsposeBarcode\Aspose.Total.lic");
+
+            foreach (var item in Directory.GetFiles(@"D:\GIT-Estudo\DocsBarcode"))
+            {
+                try
+                {
+                    string fileName = Path.GetFileName(item);
+                    //Bitmap bmpFile = new System.Drawing.Bitmap(item);
+
+                    BaseDecodeType type = null;
+                    BaseDecodeType.TryParse("CODE39", out type);
+
+                    using (BarCodeReader reader = new BarCodeReader(item, DecodeType.Code39Standard))
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Arquivo: " + fileName);
+
+                        foreach (BarCodeResult result in reader.ReadBarCodes())
+                        {
+                            Console.WriteLine("******************");
+                            Console.WriteLine("Type: " + result.CodeType);
+                            Console.WriteLine("CodeText: " + result.CodeText);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.WriteLine("Finalizado");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Zeev.AsposeBarcode/Zeev.AsposeBarcode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using Zeev.AsposeBarcode.Library;
+
+namespace Zeev.AsposeBarcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadBarcode.Read();
+        }
+    }
+}
